8d2008a4c97e2171ed68e7bcfc6e8bb1bc0690e9
Zhaoyu <weixiaobo88@gmail.com>
2019-07-08 15:23:08 +0800

    finish map_to_four_multiples_add_one

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,11 @@
-'use strict';
-var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+"use strict";
+var map_to_four_multiples_add_one = function(collection) {
+  let result = [];
+  for (let index = 0; index < collection.length; index++) {
+    const element = collection[index];
+    result.push(element * 4 + 1);
+  }
+  return result;
 };
 
 module.exports = map_to_four_multiples_add_one;
