cb1bb3750252342856a392ed2495dc834aeae1f2
Zhaoyu <weixiaobo88@gmail.com>
2019-07-08 15:21:30 +0800

    finish map_to_even

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,5 +1,11 @@
-'use strict';
-function map_to_even(collection){
-  return [2,4,6,8,10];
+"use strict";
+function map_to_even(collection) {
+  let result = [];
+  for (let index = 0; index < collection.length; index++) {
+    const element = collection[index];
+    result.push(element * 2);
+  }
+
+  return result;
 }
 module.exports = map_to_even;
