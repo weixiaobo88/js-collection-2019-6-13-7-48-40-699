be68e35f299411acac9aa017cfe945c39f94d758
Zhaoyu <weixiaobo88@gmail.com>
2019-07-08 15:18:06 +0800

    finsh grouping_count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,16 @@
-'use strict';
+"use strict";
 
 function grouping_count(collection) {
-
-  //implement here
+  let result = {};
+  for (let index = 0; index < collection.length; index++) {
+    const element = collection[index];
+    if (result[element]) {
+      result[element] = result[element] + 1;
+    } else {
+      result[element] = 1;
+    }
+  }
+  return result;
 }
 
 module.exports = grouping_count;
