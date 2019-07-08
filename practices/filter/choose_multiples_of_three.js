31293ce4f32e71a3ca9502ff8e7e16cbc5de797b
Zhaoyu <weixiaobo88@gmail.com>
2019-07-08 15:15:28 +0800

    finish choose_multiples_of_three

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 function choose_multiples_of_three(collection) {
-
-  //implement here
+  let result = [];
+  for (let index = 0; index < collection.length; index++) {
+    const element = collection[index];
+    if (element % 3 === 0) {
+      result.push(element);
+    }
+  }
+  return result;
 }
 
 module.exports = choose_multiples_of_three;
